ed0060a0507480f55e019771843a96b233e97097
Carlos Mayol (MSFT) <carlosmayol@users.noreply.github.com>
2020-12-22 12:05:02 -0500

    Update VMM2012_Get-VMMPerfData.ps1

--- VMM2012_Get-VMMPerfData.ps1
+++ VMM2012_Get-VMMPerfData.ps1
@@ -1,6 +1,6 @@
 ﻿import-module virtualmachinemanager
 
-
+#Reporting Hosts:
 $vmhosts = Get-SCVMHost 
 
 foreach ($vmhost in $vmhosts) {
@@ -30,4 +30,34 @@ $vmhostCPUTimeSamples = Get-SCVMHOST $vmhost | Get-SCPerformanceData -Performanc
     $result | ft -AutoSize #we can send this to a CSV file for graph out
 }
 
-        
+
+#Reporting VMs      
+
+$VMs= Get-SCVirtualMachine
+
+foreach ($vm in $vms) {
+
+$vmCPUPerfHistory = Get-SCVirtualMachine $vm | Get-SCPerformanceData -PerformanceCounter CPUUsage -TimeFrame Day | select -ExpandProperty PerformanceHistory
+$vmMEMPerfHistory = Get-SCVirtualMachine $vm | Get-SCPerformanceData -PerformanceCounter MemoryUsage -TimeFrame Day | select -ExpandProperty PerformanceHistory
+$vmNETIORPerfHistory = Get-SCVirtualMachine $vm | Get-SCPerformanceData -PerformanceCounter NetworkIOReceived -TimeFrame Day | select -ExpandProperty PerformanceHistory
+$vmNETIOSPerfHistory = Get-SCVirtualMachine $vm | Get-SCPerformanceData -PerformanceCounter NetworkIOSent -TimeFrame Day | select -ExpandProperty PerformanceHistory 
+$vmStoragePerfHistory = Get-SCVirtualMachine $vm | Get-SCPerformanceData -PerformanceCounter StorageIOPSUsage -TimeFrame Day | select -ExpandProperty PerformanceHistory
+$vmCPUTimeSamples = Get-SCVirtualMachine $vm | Get-SCPerformanceData -PerformanceCounter CPUUsage -TimeFrame Day | select -ExpandProperty TimeSamples
+
+[int]$max = $vmCPUTimeSamples.Count
+
+    $result = for ($i=0; $i -lt $max; $i++)
+    {
+        [PScustomObject]@{
+            Name=$vm.Name
+            CPUUsagePercent=$vmCPUPerfHistory[$i]
+            MemUsageGB=$vmMEMPerfHistory[$i]
+            NetRecMBps=$vmNETIORPerfHistory[$i]
+            NetSentMBps=$vmNETIOSPerfHistory[$i]
+            StorageMBps=$vmStoragePerfHistory[$i]
+            Time=$vmCPUTimeSamples[$i]
+            }
+    }
+    
+    $result | ft -AutoSize #we can send this to a CSV file for graph out
+}
